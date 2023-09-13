23ec0a52113ac9d4cd135da503fa02b288694dc4
workerandi <workerandiramirez@gmail.com>
2023-09-13 13:11:01 -0500

    Checking if it works
    nothing really

---
+++ weather2/index.js
@@ -0,0 +1,46 @@
+//Challenge 1
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let currentDay = days[now.getDay()];
+let currentHour = now.getHours();
+let currentMinute = now.getMinutes();
+if (currentHour < 10) {
+  currentHour = `0${currentHour}`;
+}
+if (currentMinute < 10) {
+  currentMinute = `0${currentMinute}`;
+}
+let time = document.querySelector("h2");
+
+time.innerHTML = `${currentDay}, ${currentHour}:${currentMinute}`;
+
+function updatedPage(response) {
+  document.querySelector("#h1").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#currentHigh").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  document.querySelector("#currentLow").innerHTML = Math.round(
+    response.data.main.temp_min
+  );
+}
+
+function changeCity(event) {
+  let citySearched = document.querySelector("#searchBar").value;
+  let apiKey = "2d96d64425dca1d6eda00d942a281c0d";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearched}&appid=${apiKey}`;
+  axios.get(apiUrl).then(updatedPage);
+}
+
+let searchCity = document.querySelector("#inputForm");
+searchCity.addEventListener("submit", changeCity);
