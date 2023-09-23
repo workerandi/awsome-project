e1d8c9617dc99f93f0e9563d622aebd355adf3de
workerandi <workerandiramirez@gmail.com>
2023-09-22 19:36:17 -0500

    Updated js
    Added the conversions/ update from codesandbox of (after-solution).

--- weather2/index.js
+++ weather2/index.js
@@ -1,38 +1,41 @@
-//Challenge 1
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let currentDay = days[now.getDay()];
-let currentHour = now.getHours();
-let currentMinute = now.getMinutes();
-if (currentHour < 10) {
-  currentHour = `0${currentHour}`;
+// Challenge 1
+function updateTime() {
+  let now = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = days[now.getDay()];
+  let currentHour = now.getHours();
+  let currentMinute = now.getMinutes();
+  if (currentHour < 10) {
+    currentHour = `0${currentHour}`;
+  }
+  if (currentMinute < 10) {
+    currentMinute = `0${currentMinute}`;
+  }
+  let time = document.querySelector("h2");
+  time.innerHTML = `${currentDay}, ${currentHour}:${currentMinute}`;
 }
-if (currentMinute < 10) {
-  currentMinute = `0${currentMinute}`;
-}
-let time = document.querySelector("h2");
 
-time.innerHTML = `${currentDay}, ${currentHour}:${currentMinute}`;
+// Update time initially and refresh every minute
+updateTime(event);
+event.preventDefault();
+setInterval(updateTime, 60000); // Refresh every minute
 
 function updatedPage(response) {
   document.querySelector("#h1").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#currentHigh").innerHTML = Math.round(
-    response.data.main.temp_max
-  );
-  document.querySelector("#currentLow").innerHTML = Math.round(
-    response.data.main.temp_min
-  );
+  document.querySelector("#temperature").innerHTML =
+    Math.round(((response.data.main.temp - 273.15) * 9) / 5 + 32) + "°F";
+  document.querySelector("#currentHigh").innerHTML =
+    Math.round(((response.data.main.temp_max - 273.15) * 9) / 5 + 32) + "°F";
+  document.querySelector("#currentLow").innerHTML =
+    Math.round(((response.data.main.temp_min - 273.15) * 9) / 5 + 32) + "°F";
 }
 
 function changeCity(event) {
